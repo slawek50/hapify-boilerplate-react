197338ede21a5c8367ce845de8928ce8f8258edf
Raphaël Chevallier <raphael.chevallier@gmail.com>
2020-05-06 17:44:46 -0400

    Remove classic EditForm elements

--- app/components/elements/index.js
+++ app/components/elements/index.js
@@ -1,7 +1,3 @@
 export * from 'redux-form-antd';
 
-export CheckboxGroupField from './CheckboxGroupField';
-export NumberField from './NumberField';
-export SwitchField from './SwitchField';
-
 export ArrayFieldAntd from './ArrayFieldAntd';
